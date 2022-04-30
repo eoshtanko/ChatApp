5c690ffb5d2ac12ecfa103822dee5f5ef5918067
Ekaterina Shtanko <eoshtanko@edu.hse.ru>
2022-04-30 10:36:33 +0300

    Almost done.

--- ChatApp/ChatApp/Presentation Layer/Controller/Profile/PhotoSelectionViewController.swift
+++ ChatApp/ChatApp/Presentation Layer/Controller/Profile/PhotoSelectionViewController.swift
@@ -10,53 +10,40 @@ import UIKit
 
 class PhotoSelectionViewController: UIViewController {
     
+    var photoSelectionView: PhotoSelectionView? {
+        view as? PhotoSelectionView
+    }
+    
     private var themeManager: ThemeManagerProtocol = ThemeManager(theme: .classic)
     var currentTheme: Theme = .classic {
         didSet {
             themeManager.theme = currentTheme
-            setCurrentTheme()
+            photoSelectionView?.setCurrentTheme(themeManager)
         }
     }
     
-    private var activityIndicator: UIActivityIndicatorView?
-    
+    private var currentAPICallPage = 1
     private let requestSender = RequestSender()
     
-    var photoCollectionView: UICollectionView?
     var photoesURL: [String] = []
     
-    private var currentAPICallPage = 1
-    
     var choosePhotoAction: ((String) -> Void)?
-    
-    let flowLayout: UICollectionViewFlowLayout = {
-        let layout = UICollectionViewFlowLayout()
-        layout.minimumInteritemSpacing = Const.collectionViewSpacing
-        layout.minimumLineSpacing = Const.collectionViewSpacing
-        layout.sectionInset = UIEdgeInsets(top: Const.collectionViewSpacing,
-                                           left: Const.collectionViewSpacing,
-                                           bottom: Const.collectionViewSpacing,
-                                           right: Const.collectionViewSpacing)
-        return layout
-    }()
     
     init(choosePhotoAction: ((String) -> Void)?) {
         self.choosePhotoAction = choosePhotoAction
         super.init(nibName: nil, bundle: nil)
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        photoCollectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: flowLayout)
-        configurePhotoCollection()
-        configureActivityIndicator()
-        activityIndicator?.startAnimating()
-        loadImages(page: 1, completitionSuccess: completitionSuccessForInitialRequest(_:), competitionFailer: completitionFailerForInitialRequest(_:))
+    override func loadView() {
+        view = PhotoSelectionView()
     }
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        setCurrentTheme()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        photoSelectionView?.configureView()
+        configurePhotoCollection()
+        photoSelectionView?.activityIndicator?.startAnimating()
+        downloadImages(page: 1, completitionSuccess: completitionSuccessForInitialRequest(_:), competitionFailer: completitionFailerForInitialRequest(_:))
     }
     
     func setCurrentTheme(_ theme: Theme?) {
@@ -66,12 +53,36 @@ class PhotoSelectionViewController: UIViewController {
     }
     
     private func configurePhotoCollection() {
-        if let photoCollectionView = photoCollectionView {
-            view.addSubview(photoCollectionView)
-            photoCollectionView.register(UINib(nibName: "PhotoCell", bundle: nil), forCellWithReuseIdentifier: PhotoCell.identifier)
-            photoCollectionView.delegate = self
-            photoCollectionView.dataSource = self
-            photoCollectionView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 50, right: 0)
+        photoSelectionView?.photoCollectionView?.delegate = self
+        photoSelectionView?.photoCollectionView?.dataSource = self
+    }
+    
+    private func downloadImages(page: Int, completitionSuccess: (([ImageData]) -> Void)?, competitionFailer: ((Error) -> Void)?) {
+        let requestConfig = RequestsFactory.ImageRequests.getImages(pageNumber: page)
+        requestSender.send(config: requestConfig) { (result: Result<[ImageData], Error>) in
+            switch result {
+            case .success(let imageModels):
+                completitionSuccess?(imageModels)
+            case .failure(let failure):
+                competitionFailer?(failure)
+            }
+        }
+    }
+    
+    private func completitionSuccessForInitialRequest(_ imageModels: [ImageData]) {
+        for imageModel in imageModels {
+            self.photoesURL.append(imageModel.largeImageURL)
+        }
+        DispatchQueue.main.async {
+            self.photoSelectionView?.activityIndicator?.stopAnimating()
+            self.photoSelectionView?.photoCollectionView?.reloadData()
+        }
+    }
+    
+    private func completitionFailerForInitialRequest(_ failure: Error) {
+        DispatchQueue.main.async {
+            print(failure)
+            self.photoSelectionView?.activityIndicator?.stopAnimating()
         }
     }
     
@@ -91,69 +102,18 @@ class PhotoSelectionViewController: UIViewController {
         }
     }
     
-    private func configureActivityIndicator() {
-        activityIndicator = UIActivityIndicatorView()
-        activityIndicator?.center = photoCollectionView?.center ?? view.center
-        activityIndicator?.hidesWhenStopped = true
-        activityIndicator?.style = .large
-        activityIndicator?.transform = CGAffineTransform(scaleX: 3, y: 3)
-        if let activityIndicator = activityIndicator {
-            photoCollectionView?.addSubview(activityIndicator)
-        }
-    }
-    
-    private func loadImages(page: Int, completitionSuccess: (([ImageData]) -> Void)?, competitionFailer: ((Error) -> Void)?) {
-        let requestConfig = RequestsFactory.ImageRequests.getImages(pageNumber: page)
-        requestSender.send(config: requestConfig) { (result: Result<[ImageData], Error>) in
-            switch result {
-            case .success(let imageModels):
-                completitionSuccess?(imageModels)
-            case .failure(let failure):
-                competitionFailer?(failure)
-            }
-        }
-    }
-    
-    private func completitionSuccessForInitialRequest(_ imageModels: [ImageData]) {
-        for imageModel in imageModels {
-            self.photoesURL.append(imageModel.largeImageURL)
-        }
-        DispatchQueue.main.async {
-            self.activityIndicator?.stopAnimating()
-            self.photoCollectionView?.reloadData()
-        }
-    }
-    
-    private func completitionFailerForInitialRequest(_ failure: Error) {
-        DispatchQueue.main.async {
-            print(failure)
-            self.activityIndicator?.stopAnimating()
-        }
-    }
-    
-    func setCurrentTheme() {
-        view.backgroundColor = themeManager.themeSettings?.backgroundColor
-        photoCollectionView?.backgroundColor = themeManager.themeSettings?.backgroundColor
-    }
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    enum Const {
-        static let collectionViewSpacing: CGFloat = 5
     }
 }
 
 extension PhotoSelectionViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = collectionView.bounds.width
-        let numberOfItemsPerRow: CGFloat = 3
-        let spacing: CGFloat = flowLayout.minimumInteritemSpacing
-        let availableWidth = width - spacing * (numberOfItemsPerRow + 1)
-        let itemDimension = floor(availableWidth / numberOfItemsPerRow)
-        return CGSize(width: itemDimension, height: itemDimension)
+        guard let photoSelectionView = photoSelectionView else {
+            return CGSize(width: 0, height: 0)
+        }
+        return photoSelectionView.getSizeForItemAt()
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -178,8 +138,7 @@ extension PhotoSelectionViewController: UICollectionViewDataSource {
         
         if indexPath.row == photoesURL.count - 1 {
             currentAPICallPage += 1
-            
-            self.loadImages(page: currentAPICallPage, completitionSuccess: completitionSuccessForRepeatedRequest, competitionFailer: nil)
+            self.downloadImages(page: currentAPICallPage, completitionSuccess: completitionSuccessForRepeatedRequest, competitionFailer: nil)
         }
         
         return cell
@@ -190,7 +149,7 @@ extension PhotoSelectionViewController: UICollectionViewDataSource {
             photoesURL.append(model.largeImageURL)
         }
         DispatchQueue.main.async {
-            self.photoCollectionView?.reloadData()
+            self.photoSelectionView?.photoCollectionView?.reloadData()
         }
     }
 }
--- ChatApp/ChatApp/Presentation Layer/View/Profile/PhotoSelectionView.swift
+++ ChatApp/ChatApp/Presentation Layer/View/Profile/PhotoSelectionView.swift
@@ -10,59 +10,78 @@ import UIKit
 
 class PhotoSelectionView: UIView {
     
-//    var activityIndicator: UIActivityIndicatorView?
-//
-//    var photoCollectionView: UICollectionView?
-//
-//    private let flowLayout: UICollectionViewFlowLayout = {
-//        let layout = UICollectionViewFlowLayout()
-//        layout.minimumInteritemSpacing = Const.collectionViewSpacing
-//        layout.minimumLineSpacing = Const.collectionViewSpacing
-//        layout.sectionInset = UIEdgeInsets(top: Const.collectionViewSpacing,
-//                                           left: Const.collectionViewSpacing,
-//                                           bottom: Const.collectionViewSpacing,
-//                                           right: Const.collectionViewSpacing)
-//        return layout
-//    }()
-//
-//    func configureView() {
-//        configureCollection()
-//        configurePhotoCollection()
-//        configureActivityIndicator()
-//    }
-//
-//    func getSizeForItemAt() -> CGSize {
-//        let width = photoCollectionView?.bounds.width
-//        let numberOfItemsPerRow: CGFloat = 3
-//        let spacing: CGFloat = flowLayout.minimumInteritemSpacing
-//        let availableWidth = width ?? 0 - spacing * (numberOfItemsPerRow + 1)
-//        let itemDimension = floor(availableWidth / numberOfItemsPerRow)
-//        return CGSize(width: itemDimension, height: itemDimension)
-//    }
-//
-//    private func configureCollection() {
-//        photoCollectionView = UICollectionView(frame: self.frame, collectionViewLayout: flowLayout)
-//    }
-//
-//    private func configurePhotoCollection() {
-//        if let photoCollectionView = photoCollectionView {
-//            self.addSubview(photoCollectionView)
-//            photoCollectionView.register(UINib(nibName: "PhotoCell", bundle: nil), forCellWithReuseIdentifier: PhotoCell.identifier)
-//        }
-//    }
-//
-//    private func configureActivityIndicator() {
-//        activityIndicator = UIActivityIndicatorView()
-//        activityIndicator?.center = photoCollectionView?.center ?? center
-//        activityIndicator?.hidesWhenStopped = true
-//        activityIndicator?.style = .large
-//        activityIndicator?.transform = CGAffineTransform(scaleX: 3, y: 3)
-//        if let activityIndicator = activityIndicator {
-//            photoCollectionView?.addSubview(activityIndicator)
-//        }
-//    }
-//
-//    enum Const {
-//        static let collectionViewSpacing: CGFloat = 5
-//    }
+    var activityIndicator: UIActivityIndicatorView?
+
+    var photoCollectionView: UICollectionView?
+
+    private let flowLayout: UICollectionViewFlowLayout = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumInteritemSpacing = Const.collectionViewSpacing
+        layout.minimumLineSpacing = Const.collectionViewSpacing
+        layout.sectionInset = UIEdgeInsets(top: Const.collectionViewSpacing,
+                                           left: Const.collectionViewSpacing,
+                                           bottom: Const.collectionViewSpacing,
+                                           right: Const.collectionViewSpacing)
+        return layout
+    }()
+
+    func configureView() {
+        createCollection()
+        configureActivityIndicator()
+        configurePhotoCollection()
+    }
+
+    func getSizeForItemAt() -> CGSize {
+        let numberOfItemsPerRow: CGFloat = 3
+        let spacing: CGFloat = flowLayout.minimumInteritemSpacing
+        let availableWidth = bounds.width - spacing * (numberOfItemsPerRow + 1)
+        let itemDimension = floor(availableWidth / numberOfItemsPerRow)
+        return CGSize(width: itemDimension, height: itemDimension)
+    }
+    
+    func setCurrentTheme(_ themeManager: ThemeManagerProtocol) {
+        self.backgroundColor = themeManager.themeSettings?.backgroundColor
+        photoCollectionView?.backgroundColor = themeManager.themeSettings?.backgroundColor
+    }
+    
+    private func createCollection() {
+        photoCollectionView = UICollectionView(frame: self.frame, collectionViewLayout: flowLayout)
+    }
+    
+    private func configurePhotoCollection() {
+        if let photoCollectionView = photoCollectionView {
+            self.addSubview(photoCollectionView)
+            photoCollectionView.register(UINib(nibName: "PhotoCell", bundle: nil), forCellWithReuseIdentifier: PhotoCell.identifier)
+            photoCollectionView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 50, right: 0)
+            configureTableViewAppearance()
+        }
+    }
+    
+    private func configureTableViewAppearance() {
+        if let photoCollectionView = photoCollectionView {
+            NSLayoutConstraint.activate([
+                photoCollectionView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+                photoCollectionView.topAnchor.constraint(equalTo: self.topAnchor),
+                photoCollectionView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+                photoCollectionView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+            ])
+            
+            photoCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        }
+    }
+    
+    private func configureActivityIndicator() {
+        activityIndicator = UIActivityIndicatorView()
+        activityIndicator?.center = CGPoint(x: UIScreen.main.bounds.size.width / 2, y: UIScreen.main.bounds.size.height / 2)
+        activityIndicator?.hidesWhenStopped = true
+        activityIndicator?.style = .large
+        activityIndicator?.transform = CGAffineTransform(scaleX: 3, y: 3)
+        if let activityIndicator = activityIndicator {
+            photoCollectionView?.addSubview(activityIndicator)
+        }
+    }
+
+    private enum Const {
+        static let collectionViewSpacing: CGFloat = 5
+    }
 }
